0a7ff20060525a4ed6204d7e45898607d4c27cc0
Markio125 <nayandeepdeb125@gmail.com>
2025-03-25 00:17:32 +0530

    NCERT data added

--- knowledge_base/data_processor.py
+++ knowledge_base/data_processor.py
@@ -1,4 +1,4 @@
-import fitz  # PyMuPDF
+import pymupdf
 import json
 import os
 import re
@@ -14,7 +14,7 @@ class PDFExtractor:
 
     def extract_text(self):
         """Extracts text from a PDF and returns a structured chapter-wise format."""
-        doc = fitz.open(self.pdf_path)
+        doc = pymupdf.open(self.pdf_path)
         full_text = ""
 
         for page in doc:
@@ -50,7 +50,7 @@ class TextProcessor:
     def split_into_sections(text):
         """Splits text into equal sections based on num_sections."""
         words = text.split()
-        num_sections = len(words) // 1000   #each section will contain roughly 1000 words
+        num_sections = len(words) // 500   #each section will contain roughly 1000 words
         chunk_size = len(words) // num_sections
         sections = []
 
@@ -110,9 +110,9 @@ class PDFProcessor:
 
 
 # Example Usage
-pdf_file = "pdf_storage/sample.pdf"  # Replace with file path
+pdf_file = "pdf_storage\Chapter 11_ Consumer Protection.pdf"  # Replace with file path
 json_file = "business_studies.json"
-chapter_name = "Introduction to AI"  # chapter name
+chapter_name = "Consumer Protection"  # chapter name
 
 processor = PDFProcessor(pdf_file, json_file, chapter_name)
 processor.process()
